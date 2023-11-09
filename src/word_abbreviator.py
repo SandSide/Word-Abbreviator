9e7f35d095581e4b0c602df32babe441d0ed4040
SandSide <37347875+SandSide@users.noreply.github.com>
2023-11-09 16:48:27 +0000

    feat: find position of abbr chars sequency and relative to the word in are a part of

--- src/word_abbreviator.py
+++ src/word_abbreviator.py
@@ -1,6 +1,34 @@
 import re
 
-pattern = r'\W'
+charScore = {
+    'A': 25,
+    'B': 8,
+    'C': 8,
+    'D': 9,
+    'E': 35,
+    'F': 7,
+    'G': 9,
+    'H': 7,
+    'I': 25,
+    'J': 3,
+    'K': 6,
+    'L': 15,
+    'M': 8,
+    'N': 15,
+    'O': 20,
+    'P': 8,
+    'Q': 1,
+    'R': 15,
+    'S': 15,
+    'T': 15,
+    'U': 20,
+    'V': 7,
+    'W': 7,
+    'X': 3,
+    'Y': 7,
+    'Z': 1
+}
+
 
 def load_file(filename):
 ### Load file lines into a list ###
@@ -91,12 +119,140 @@ def remove_duplicates(abbrLists, uniqueAbbrLists):
               
     return [list(set(x) - duplicates) for x in abbrLists]
 
-def score_abbreviations(abbreviationLists):
-    v = 1
+def score_abbreviations(abbrLists, wordLists):
+    
+    for abbreviations, words in zip(abbrLists,wordLists):
+        
+        scores = []
+        
+        print(words)
+        
+        for abbr in abbreviations:
+            print(determine_abbr_positions(abbr, words))    
+
+            
+                
+            
+def determine_abbr_positions(abbr, words):
+                       
+    abbrPos = []
+    currChar = abbr[1]
+    currPos = 0
+    currWordIndex = 0
+    currWord = words[currWordIndex]
+    
+    print(abbr)
+    
+    while(True):
+        if currWordIndex == len(words):
+            break
+        
+        currWord = words[currWordIndex]
+        
+        if currPos == len(currWord):
+            currWordIndex += 1
+            currPos = 0
+            continue  
+        elif currChar == currWord[currPos]:
+            abbrPos.append((currChar, currPos))
+            
+            currChar = abbr[2]
+            currPos += 1           
+        else:
+            currPos += 1 
+            
+    return abbrPos 
+                
+                        # firstChar = [x[0] for x in words]
+            # lastChar = [x[-1] for x in words]
+                                            
+            # for c in abbr:
+                
+
+
+            #     while(True):
+            #         if currPos == len(currWord):
+            #             print('End at {}'.format(currPos))
+            #             currWordIndex += 1
+            #             currWord = words[currWordIndex]
+            #             currPos = 0
+            #             break
+            #         elif c == currWord[currPos]:
+            #             print('{} found at {}'.format(c,currPos))
+            #             currPos += 1
+            #             break
+                        
+            #         else:
+            #             currPos += 1
+
+            #     continue
+                
+                # while(True):
+                #     if currPos == len(currWord):
+                #         currWordIndex += 1
+                #         currPos = 0
+                #         print(currWordIndex)
+                #         currWord = words[currWordIndex]
+                    
+                #     elif c == currWord[currPos]:
+                #         print(currPos)
+                #         currPos += 1
+                #         break
+                    
+                #     else:
+                #         currPos += 1
+                    
+                    
+                
+            
+            
+            
+            # Find all positions, posiiton is sequencial
+            
+            # pos = [[words.find(char) for char in abbr]]
+        
+            # print(pos)
+            
+            
+            # positions = [(c,i) for ]
+            
+            
+            # print('{}.....{}'.format(firstChar,lastChar))
+            
+            #score = 0
+            
+            # for c in abbr:
+                
+            #     if c == abbr[0]:
+            #         continue
+                          
+            #     if c == firstChar:
+            #         score += 0
+            #     elif c == lastChar:
+            #         score += 20 if c == 'E' else 5
+            #     else:
+            #         # Commonality score
+            #         score += charScore[c]
+                    
+                    # 2,3,4
+                    
+                    
+                    
+                    # Position score
+                    
+                      
+
+        #     scores.append((abbr, score))
+         
+        # for x in scores:
+        #     print(x)   
+    
     
 words = load_file('test')
 splitWords = split_words(words)
-abbreviations = find_abbreviations(splitWords)
-abbreviations = cleanup_abbreviations(abbreviations)
-for x in abbreviations:
-    print(x)
+abbrLists = find_abbreviations(splitWords)
+abbrLists = cleanup_abbreviations(abbrLists)
+scores = score_abbreviations(abbrLists, splitWords) 
+
+# for x in abbreviations:
+#     print(x)
