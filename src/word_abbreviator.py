cb2afc885d71f2e3d736a089f803577cd3c2f101
SandSide <37347875+SandSide@users.noreply.github.com>
2023-11-10 23:14:55 +0000

    feat: score each abbr

--- src/word_abbreviator.py
+++ src/word_abbreviator.py
@@ -140,58 +140,82 @@ def remove_duplicates(abbr_lists, pos_lists):
         temp_abbr_list = [v for i,v in enumerate(abbr_list) if i not in duplicate_indexes]
         temp_pos_list = [v for i,v in enumerate(pos_list) if i not in duplicate_indexes]
         
-        print(temp_abbr_list)
-        
         new_abbr_lists.append(temp_abbr_list)
         new_pos_lists.append(temp_pos_list)
         
     return new_abbr_lists, new_pos_lists
 
-def score_abbreviations(abbrLists, wordLists):
+def score_abbreviations(abbr_lists, pos_type_lists):
     
-    minScores = []
-    
-    for abbreviations, words in zip(abbrLists, wordLists):
+    for abbr_list, pos_type_list in zip(abbr_lists, pos_type_lists):
         
-        scores = []  
-              
-        for abbr in abbreviations:
-            
-            abbrPos = determine_abbr_positions(abbr, words)
-            
-            score = 0
-            
-            for char, pos in zip(abbr[1:3], abbrPos):
-                
-                # print('{} {}'.format(char, pos))
+        scores = []
+        
+        for abbr, pos_types in zip(abbr_list, pos_type_list):
 
-                if pos[0] == 0: # First letter of a word
-                    score += 0
-                elif pos[1] == True: # Last Letter
-                    score += 20 if char == 'E' else 5
+            score = 0
+
+            for char, pos_type in zip (abbr[1:3], pos_types):
+                
+                char_score = 0
+                       
+                if pos_type == 'first':
+                    char_score += 0
+                elif pos_type == 'last':
+                    char_score += 20 if char == 'E' else 5
                 else:
-                    
-                    # Position Score
-                    score += score_position(pos[0])
-                    
-                    # Commonality score
-                    score += charScore[char]
-                    
-            scores.append((abbr, score))
+                    char_score += score_position(pos_type)
+                    char_score += charScore[char]
             
-        minScore = min(scores, key=lambda x: x[1])
+                score += char_score              
+                    
+            scores.append((abbr, score, pos_types))
+            
+        print(min(scores, key = lambda x: x[1]))
+                    
+                
+            
+            
+            
+          
+              
+        # for abbr in abbreviations:
+            
+        #     abbrPos = determine_abbr_positions(abbr, words)
+            
+        #     score = 0
+            
+        #     for char, pos in zip(abbr[1:3], abbrPos):
+                
+        #         # print('{} {}'.format(char, pos))
+
+        #         if pos[0] == 0: # First letter of a word
+        #             score += 0
+        #         elif pos[1] == True: # Last Letter
+        #             score += 20 if char == 'E' else 5
+        #         else:
+                    
+        #             # Position Score
+        #             score += score_position(pos[0])
+                    
+        #             # Commonality score
+        #             score += charScore[char]
+                    
+        #     scores.append((abbr, score))
+            
+        # minScore = min(scores, key=lambda x: x[1])
         
-        minScores.append(minScore) 
+        # minScores.append(minScore) 
         
-    return minScores
+    return 1
  
-def score_position(pos):
+def score_position(pos_type):
     
-    if pos == 1:
+    if pos_type == 'second':
         return 1
-    elif pos == 2:
+    elif pos_type == 'third':
         return 2
-    elif pos >= 3:
+    elif pos_type == 'middle':
         return 3
     
     return 0
@@ -228,35 +252,24 @@ def determine_abbr_positions(abbr, words):
     
     
 
-words = load_file('test')
+words = load_file('oop')
 split_words_lists = split_words(words)
 
 abbr_lists, abbr_pos__type_lists = find_abbreviations(split_words_lists)
 unique_abbr_lists, unique_pos_lists = remove_duplicates(abbr_lists, abbr_pos__type_lists)
 
-temp = unique_abbr_lists
-temp_2 = unique_pos_lists
+# print(unique_abbr_lists)
 
-# pos_type_lists = determine_abbr_pos_type(abbr_pos_lists, split_words_lists)
+# print('OAG' in unique_abbr_lists)
 
-# print(abbr_lists)
+score_abbreviations(unique_abbr_lists, unique_pos_lists)
 
+# temp = unique_abbr_lists
+# temp_2 = unique_pos_lists
 
-# print(len())
+# temp = [y for x in unique_abbr_lists for y in x if y[1] == 'A']
+# temp = unique_abbr_lists
 
-# for x in abbr_pos__type_lists:
-#     print(x)
-    
-# for x,y in zip(abbr_lists, abbr_pos__type_lists):
-#     print(len(x) == len(y))
-
-# abbrLists = cleanup_abbreviations(abbrLists)
-# scores = score_abbreviations(abbrLists, splitWords) 
-
-# print(abbrLists)
-
-# for x in abbrLists:
+# for x in temp:
 #     print(x)
 
-# for x in abbreviations:
-#     print(x)cd 
